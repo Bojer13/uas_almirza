f16a1d70c7467225706e29a9ae7c5669879f4fb0
Bojer13 <108806140+Bojer13@users.noreply.github.com>
2022-07-20 18:43:13 +0700

    migrasi dan page content product

---
+++ resources/views/product.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+
+@section('content')
+<style type="text/css">
+    .product{
+        text-align: center;
+        padding-top: 40px;
+    }
+</style>
+<div class="content-header">
+    <div class="container-fluid">
+      <div class="row mb-2">
+        <div class="col-sm-6">
+          <h1 class="m-0">Product</h1>
+      </div><!-- /.row -->
+    </div><!-- /.container-fluid -->
+  </div>
+  <div class="product">
+    <h4>add product</h4>
+    
+</div>
+</div>
+@endsection
